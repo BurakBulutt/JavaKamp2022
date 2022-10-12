aed837a44889c78d886949f8678ca0ae87725f4a
Burak Bulut <lotheas01@gmail.com>
2022-10-12 19:29:30 +0300

    3.hafta odevi eklendi

---
+++ Odev01/src/Company.java
@@ -0,0 +1,21 @@
+public class Company extends Customer{
+
+    private String taxNumber;
+    private String CompanyName;
+
+    public String getCompanyName() {
+        return CompanyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        CompanyName = companyName;
+    }
+
+    public String getTaxNumber() {
+        return taxNumber;
+    }
+
+    public void setTaxNumber(String taxNumber) {
+        this.taxNumber = taxNumber;
+    }
+}
